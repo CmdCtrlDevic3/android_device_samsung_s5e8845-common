56526316a9215f8557e32d20ab0e335b047de4ad
ExtremeXT <extremext@lineageos.org>
2025-11-03 17:57:49 +0200

    s5e8845: Import empty VINTF Manifests
    * Use target level 8 as it is what s5e8845 shipped with
    
    Change-Id: I1ef35ac541ef3950827189cd2df0762e1fc3838e
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -4,3 +4,8 @@
 #
 
 COMMON_PATH := device/samsung/s5e8845-common
+
+# VINTF
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml
