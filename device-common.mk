66ed09c61bd5511016983798f1c1ad5fe992f4ec
Secondarian <robertanitei@tutanota.com>
2026-04-20 16:09:40 +0200

    s5e8845: Inherit common makefiles
    Change-Id: I49cef9db7c670b841055f46efab5df78e152b603
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- device-common.mk
+++ device-common.mk
@@ -5,6 +5,24 @@
 
 # All components inherited here go to system image
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_system.mk)
+
+# All components inherited here go to system_ext image
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+
+# All components inherited here go to product image
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
+
+# All components inherited here go to vendor image
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/media_vendor.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# Inherit common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Permissions
+PRODUCT_PACKAGES += handheld_core_hardware.prebuilt.xml
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
