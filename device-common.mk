0336120ac14dbbff97f2fb8354d3ff2c0e14d2d6
Anthony Adamo <quic_aadamo@quicinc.com>
2026-07-01 12:58:55 +0300

    s5e8845: Inherit vabc_features.mk and update compression to lz4
    inherit vabc_features.mk instead of android_t_baseline.mk which has correct configuration for android_u.
    Update the PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD from gz to lz4.
    Also, enable virtual_ab.compression.threads to improve OTA performance.
    
    Change-Id: I3b17e236701cefab4f424c8babb2fc59d2b9df09
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -22,6 +22,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 $(call inherit-product, hardware/samsung_slsi-linaro/config/config.mk)
 $(call inherit-product, hardware/samsung_slsi-linaro/graphics/base/hwcomposer_property.mk)
 
+# Enable virtual A/B with vendor ramdisk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
+
 # Enforce generic ramdisk allow list
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
@@ -44,6 +47,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
 PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
+
+PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
