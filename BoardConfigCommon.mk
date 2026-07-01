27504a3577c17353a3bbc5bdc7c939a29e1f8fbb
fiqys <elsifycritic@gmail.com>
2026-07-01 12:29:35 +0300

    s5e8845: Define platform

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -5,6 +5,19 @@
 
 COMMON_PATH := device/samsung/s5e8845-common
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a76
+
+# Platform
+BOARD_VENDOR := samsung
+TARGET_BOARD_PLATFORM := erd8845
+TARGET_BOOTLOADER_BOARD_NAME := s5e8845
+TARGET_SOC := s5e8845
+
 # Security
 VENDOR_SECURITY_PATCH := 2026-07-05
 
