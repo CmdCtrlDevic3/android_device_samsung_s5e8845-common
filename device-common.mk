fd8c25469da844df7002066be2946b8136e446c9
Tim Zimmerman <tim@linux4.de>
2026-07-01 12:36:35 +0300

    s5e8845: Switch to F2FS with fscrypt v2 for /data
    * Since users will be forced to wipe after the android-4.14-stable merge anyway, switch to a more future proof encryption setup
    
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Kernel Modules
 PRODUCT_PACKAGES += \
     linker.vendor_ramdisk \
