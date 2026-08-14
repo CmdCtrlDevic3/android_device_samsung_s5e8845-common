fb2ecce18f0804b38c540196d800a69979965a47
Tim Zimmermann <tim@linux4.de>
2026-08-14 16:06:02 +0300

    s5e8845: Import missing namespaces
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -142,6 +142,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/samsung
 
 # Linker
