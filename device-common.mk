a2de763e78bbce010727aba0dcb739270e3d397a
luk1337 <priv.luk@gmail.com>
2026-08-14 13:12:39 +0300

    s5e8845: Add A/B updates support

--- device-common.mk
+++ device-common.mk
@@ -28,6 +28,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 # Inherit common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vbmeta
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=erofs \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
@@ -43,6 +58,15 @@ PRODUCT_PACKAGES += handheld_core_hardware.prebuilt.xml
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Update Engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
 # Call the proprietary setup
 $(call inherit-product, vendor/samsung/s5e8845-common/s5e8845-common-vendor.mk)
