6e6b7b6515b434276a5bab59061ec6331b869f92
Tim Zimmermann <tim@linux4.de>
2026-08-14 16:04:59 +0300

    s5e8845: build AIDL camera HAL
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -63,6 +63,10 @@ PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
     media_codecs_performance_c2.xml \
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider-service.samsung
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
