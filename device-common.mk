25969b9c73fe3dd2f177ad6dc50d507d94855a99
Stricted <info@stricted.net>
2026-08-14 12:58:53 +0300

    s5e8845: Inherit Samsung LSI board support package config
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -18,3 +18,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Call the proprietary setup
 $(call inherit-product, vendor/samsung/s5e9925-common/s5e8845-common-vendor.mk)
+
+# Call Samsung LSI board support package makefiles
+$(call inherit-product, hardware/samsung_slsi-linaro/config/config.mk)
+$(call inherit-product, hardware/samsung_slsi-linaro/graphics/base/hwcomposer_property.mk)
