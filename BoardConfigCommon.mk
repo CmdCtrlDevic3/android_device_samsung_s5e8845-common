e7f9d445de4b13f8068e3330d2005f16e886e386
fiqys <elsifycritic@gmail.com>
2026-08-14 12:26:46 +0300

    s5e8845: Setup VINTF Manifests
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -7,3 +7,9 @@ COMMON_PATH := device/samsung/s5e8845-common
 
 # Security
 VENDOR_SECURITY_PATCH := 2026-07-05
+
+# VINTF
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(COMMON_PATH)/configs/vintf/compatibility_matrix.device.xml \
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml
