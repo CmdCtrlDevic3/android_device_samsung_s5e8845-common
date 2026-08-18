4a28d6aee791d85753dd1abc3a1ecbc9bad8fb4a
sec0ndarian <robertanitei@tutanota.com>
2026-08-18 14:56:52 +0300

    s5e8845: Call Renamed libcrypto Symbols Directly
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -4,9 +4,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+    run_cmd
+)
+
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
+)
+
+from extract_utils.tools import (
+    DEFAULT_PATCHELF_VERSION,
+    patchelf_version_path_map,
 )
 
 namespace_imports = [
@@ -17,12 +28,34 @@ namespace_imports = [
     'hardware/samsung_slsi-linaro/sgpu',
 ]
 
+def rename_dynamic_symbol(
+    _ctx: BlobFixupCtx,
+    _file: File,
+    file_path: str,
+    old_name: str,
+    new_name: str,
+    **_kwargs,
+):
+    with tempfile.NamedTemporaryFile(mode='w', encoding='utf-8') as tmp:
+        tmp.write(f'{old_name} {new_name}')
+        tmp.flush()
+        run_cmd(
+            [
+                patchelf_version_path_map[DEFAULT_PATCHELF_VERSION],
+                '--rename-dynamic-symbols',
+                tmp.name,
+                file_path,
+            ]
+        )
+
 blob_fixups: blob_fixups_user_type = {
     'vendor/bin/hermesd': blob_fixup()
         .binary_regex_replace(b'security.securehw.available', b'vendor.s.securehw.available')
         .binary_regex_replace(b'security.securenvm.available', b'vendor.s.securenvm.available'),
     'vendor/lib64/libskeymint_cli.so': blob_fixup()
-        .add_needed('libshim_crypto.so'),
+        .call(rename_dynamic_symbol, 'OPENSSL_sk_delete', 'sk_delete')
+        .call(rename_dynamic_symbol, 'OPENSSL_sk_dup', 'sk_dup')
+        .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
     'vendor/etc/init/android.hardware.security.keymint-service.samsung.rc': blob_fixup()
         .regex_replace('-service', '-service.samsung'),
 }  # fmt: skip
